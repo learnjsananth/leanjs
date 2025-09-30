87b76a1d52711e885bf7a0a42ad916c3c8a81d8e
learnjsananth <learnjsananth@gmail.com>
2025-09-30 00:25:43 -0400

    undefinedvsnotdefined
    undefinedvsnotdefined

---
+++ undefinedVSNotdefined.js
@@ -0,0 +1,34 @@
+//undefined vs Not defined
+
+//Even before code is executed , global execution is context is created
+
+//and memory  is allocated
+
+var a = 7
+//even before code is executed , a is given memory space
+// a is undefined
+//once cpde is executed , a = 7
+
+console.log(x) //not defined - we have not initialized
+
+var b ;
+
+if(b==undefined){
+  console.log("undefined")
+}else{
+  console.log("not undefined")
+}
+
+//Javascript is loosely coupled
+// if a is 7 , later it can be string be number or boolean
+
+// if its strict type , it cannot change to number or boolean
+
+//a=undefined // dont do this .undefined is a place holder
+//console.log(a)  undeined
+
+
+
+
+
+
